70c3d7b9f94952e5523bbf281b95853fccd5da13
Derek DeVries <devrieda@gmail.com>
2013-01-08 14:33:09 -0700

    require test request for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 
 require File.expand_path('../../lib/color_parser.rb', __FILE__)
+require File.expand_path('../../test/test_request.rb', __FILE__)
 
 include ColorParser
