465d10ee89db25d797e0ab511d97c780cb8d66bb
Derek DeVries <devrieda@gmail.com>
2013-04-30 09:33:59 -0600

    finish converting test to spec

--- test/test_helper.rb
+++
@@ -1,6 +0,0 @@
-require 'minitest/autorun'
-
-require File.expand_path('../../lib/color_parser.rb', __FILE__)
-require File.expand_path('../../test/test_request.rb', __FILE__)
-
-include ColorParser
